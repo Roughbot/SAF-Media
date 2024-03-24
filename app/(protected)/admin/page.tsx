f105b17d16993cc475d36c948ccaab10394817b4
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-03-24 23:34:52 +0530

    dashboard

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <div>
+      <div className="grid grid-cols-3 gap-6 pt-10 px-6">
+        <div className="bg-white p-6 rounded-xl shadow-lg m-3">
+          <h1>Number of Posts</h1>
+        </div>
+        <div className="bg-white p-6 rounded-xl shadow-lg m-3">
+          <h1>Number of views</h1>
+        </div>
+        <div className="bg-white p-6 rounded-xl shadow-lg m-3">
+          <h1>Number of comments</h1>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 gap-6 pt-10 px-6">
+        <div className="bg-white p-6 rounded-xl shadow-lg m-3">
+          <h1>Recent Posts</h1>
+        </div>
+      </div>
+    </div>
   );
 }
--- app/(protected)/admin/users/page.tsx
+++ app/(protected)/admin/users/page.tsx
@@ -3,15 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <div>
-        <h1>Number of Posts</h1>
-      </div>
-      <div>
-        <h1>Number of views</h1>
-      </div>
-      <div>
-        <h1>Number of comments</h1>
-      </div>
+      <h2>Users</h2>
     </div>
   );
 };
