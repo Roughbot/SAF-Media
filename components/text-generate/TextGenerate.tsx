ea2809b9f8fb8cd5795f6c5360625164276f7e56
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-04-24 16:01:25 +0530

    about content

--- components/text-generate/TextGenerate.tsx
+++ components/text-generate/TextGenerate.tsx
@@ -1,12 +1,7 @@
 "use client";
 import { TextGenerateEffect } from "../ui/text-generate-effect";
 
-const words = `At Right Hand Venture, we are dedicated to empowering businesses like yours to reach new heights through
-the magic of digital marketing. With our personalized solutions, we ensure that your individual
-goals and requirements are not just met, but exceeded. Our team is truly passionate about
-creating strategies that perfectly align with your needs and resources. We stay at the forefront of
-industry innovations and trends to guarantee that you always receive the best possible service.
-Your success is our ultimate priority, and we are thrilled to be on this journey with you!
+const words = `What sets us apart? Our commitment to excellence, transparency, and collaboration. We don't just deliver campaigns; we build lasting relationships based on trust and mutual success. When you partner with us, you gain a dedicated team that is as invested in your success as you are. We are not just like any other digital marketing agency in the market, we don’t offer any blind services. Primarily we gain certain level of understanding of your particular company and we will give you the relevant services based on the analysed data of respective company.
 `;
 
 export function TextGenerateEffectComponent() {
