6911fec423bde9e2710bd3db1d531c1860f5578a
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-05-01 16:17:40 +0530

    work details

--- components/WorkComponent/WorkComponent.tsx
+++ components/WorkComponent/WorkComponent.tsx
@@ -2,10 +2,63 @@ import Image from "next/image";
 import "./workStyles.scss";
 import images from "@/public/assets/services.jpg";
 
+const workData = [
+  {
+    title: "My Title 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
+  },
+  {
+    title: "My Title 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
+  },
+  {
+    title: "My Title 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
+  },
+  {
+    title: "My Title 4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
+  },
+  {
+    title: "My Title 5",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
+  },
+  {
+    title: "My Title 6",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
+  },
+];
+
 const WorkComponent = () => {
   return (
     <div>
-      <section>
+      {workData.map((data, index) => (
+        <section key={index}>
+          <a href="#" className="container">
+            <div className="card">
+              <div className="content">
+                <h3 className="title">{data.title}</h3>
+
+                <div className="text">
+                  <p>{data.description}</p>
+                </div>
+                <span className="text-lg font-bold">^</span>
+              </div>
+            </div>
+            <figure>
+              <Image alt="image1" width={300} height={200} src={images} />
+            </figure>
+          </a>
+        </section>
+      ))}
+
+      {/* <section>
         <a href="#" className="container">
           <div className="card">
             <div className="content">
@@ -17,14 +70,14 @@ const WorkComponent = () => {
                   tristique ut dolor non aliquam. Donec.
                 </p>
               </div>
-              <span className="text-2xl font-bold">^</span>
+              <span className="text-lg font-bold">^</span>
             </div>
           </div>
           <figure>
             <Image alt="image1" width={300} height={200} src={images} />
           </figure>
         </a>
-      </section>
+      </section> */}
     </div>
   );
 };
