15f5aaeaf9b9509d3e27f71ba8dd31c492b38e07
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-05-26 21:05:36 +0530

    video integration

--- components/WorkComponent/WorkComponent.tsx
+++ components/WorkComponent/WorkComponent.tsx
@@ -1,47 +1,54 @@
-import Image from "next/image";
+"use client";
+import Modal from "react-modal";
+import React, { useEffect, useState } from "react";
 import "./workStyles.scss";
-import images from "@/public/assets/services.jpg";
+import image from "../../public/assets/services.jpg";
+import Image from "next/image";
 
 const workData = [
   {
     title: "My Title 1",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
+    video: "AwclCGUMrTY",
   },
   {
     title: "My Title 2",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
+    video: "hJDwkDjCld8",
   },
   {
     title: "My Title 3",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
+    video: "DLRUn4DAvdQ",
   },
   {
     title: "My Title 4",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
-  },
-  {
-    title: "My Title 5",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
-  },
-  {
-    title: "My Title 6",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ut dolor non aliquam. Donec.",
+    video: "7KG_R-i432I",
   },
 ];
 
 const WorkComponent = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div>
       {workData.map((data, index) => (
         <section key={index}>
           <a href="#" className="container">
-            <div className="card">
+            <div className="card text-black">
               <div className="content">
                 <h3 className="title">{data.title}</h3>
 
@@ -51,9 +58,37 @@ const WorkComponent = () => {
                 <span className="text-lg font-bold">^</span>
               </div>
             </div>
-            <figure>
-              <Image alt="image1" width={300} height={200} src={images} />
+            <figure onClick={openModal}>
+              <Image alt="image1" width={300} height={200} src={image} />
             </figure>
+            <Modal
+              isOpen={modalIsOpen}
+              onRequestClose={closeModal}
+              contentLabel="My dialog"
+              style={{
+                overlay: {
+                  backgroundColor: "rgba(0,0,0,0.5)",
+                },
+                content: {
+                  top: "50%",
+                  left: "50%",
+                  right: "auto",
+                  bottom: "auto",
+                  marginRight: "-50%",
+                  transform: "translate(-50%, -50%)",
+                  backgroundColor: "transparent",
+                },
+              }}
+            >
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${data.video}`}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </Modal>
           </a>
         </section>
       ))}
--- components/stickScrollReveal/StickyScrollReveal.tsx
+++ components/stickScrollReveal/StickyScrollReveal.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { StickyScroll } from "../ui/sticky-scroll-view";
 import Image from "next/image";
+import { whatWeDoContent } from "@/public/assets/whatwedo";
 
 const content = [
   {
@@ -9,8 +10,17 @@ const content = [
     description:
       "At Right Hand Venture we don't just offer marketing solutions; we pour our hearts into empowering your business to thrive the digital landscape. Our passion drives us to go above and beyond to help you achieve your goals.",
     content: (
-      <div className="h-full w-full  flex items-center justify-center text-black bg-white">
-        Passionate about Your Success
+      <div className="h-full w-full relative flex items-center justify-center text-black bg-white">
+        <div className="absolute z-10 text-3xl font-semibold text-yellow-500">
+          Passionate about Your Successz
+        </div>
+        <Image
+          src={whatWeDoContent.passionate}
+          width={300}
+          height={400}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
       </div>
     ),
   },
@@ -19,8 +29,17 @@ const content = [
     description:
       "We are not just trend followers; we are trendsetters. Constantly innovating and staying ahead of industry trends, we bring you cutting-edge marketing solutions that are tailored to drive your business forward.",
     content: (
-      <div className="h-full w-full  flex items-center justify-center text-white">
-        Innovative Solutions
+      <div className="h-full w-full relative flex items-center justify-center text-black bg-white">
+        <div className="absolute z-10 text-4xl font-semibold">
+          Inavative Solution
+        </div>
+        <Image
+          src={whatWeDoContent.innovation}
+          width={300}
+          height={400}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
       </div>
     ),
   },
@@ -29,8 +48,17 @@ const content = [
     description:
       "Your success is our success. We believe in working hand-in-hand with our clients to understand  their unique needs and craft personalized solutions that resonate with their audience and elevate their brand.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
-        Collaboration
+      <div className="h-full w-full relative flex items-center justify-center text-black">
+        <div className="absolute z-10 text-4xl font-semibold">
+          Collaboration
+        </div>
+        <Image
+          src={whatWeDoContent.collaboration}
+          width={300}
+          height={400}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
       </div>
     ),
   },
@@ -39,8 +67,14 @@ const content = [
     description:
       "Honesty, transparency, and integrity are not just words for us; they are the pillars of our    operations. You can trust Right Hand Venture to always act with integrity in all our interactions and relationships, ensuring a partnership built on trust and reliability.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-        Integrity
+      <div className="h-full w-full relative flex items-center justify-center text-black">
+        <Image
+          src={whatWeDoContent.integrity}
+          width={300}
+          height={400}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
       </div>
     ),
   },
