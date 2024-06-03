90edb82284c7f68fa1f2e05c13b3a2f2f6fbc741
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-06-03 20:18:23 +0530

    icon update

--- components/company/data.tsx
+++ components/company/data.tsx
@@ -3,10 +3,10 @@ import figma from "@/public/assets/companyLogo/figma.svg";
 import hilux from "@/public/assets/companyLogo/hilux.svg";
 import netflix from "@/public/assets/companyLogo/netflix.svg";
 import stripe from "@/public/assets/companyLogo/stripe.svg";
-import angrylabrl from "@/public/assets/companyLogo/angrylabrl.jpg";
-import animatrix from "@/public/assets/companyLogo/animatrix.jpg";
-import frshmoo from "@/public/assets/companyLogo/frshmoo.jpg";
-import untrain from "@/public/assets/companyLogo/Untrain.jpg";
+import angrylabrl from "@/public/assets/companyLogo/angrylabel.png";
+import animatrix from "@/public/assets/companyLogo/animatrix .png";
+import frshmoo from "@/public/assets/companyLogo/frshmoo.png";
+import untrain from "@/public/assets/companyLogo/Untrain.png";
 import uicons from "@/public/assets/companyLogo/uicons.png";
 
 export const companyData = [
